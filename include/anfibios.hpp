2986e25fe6204a111357c8228c0ef13483d89e3a
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2020-11-19 20:06:15 -0300

    Add classes

---
+++ include/anfibios.hpp
@@ -0,0 +1,10 @@
+#ifndef _ANFIBIOS_H_
+#define _ANFIBIOS_H_
+
+#include "animal.hpp"
+
+class Anfibios : public Animal{
+
+
+
+};
---
+++ include/repteis.hpp
@@ -0,0 +1,10 @@
+#ifndef _REPTEIS_H_
+#define _REPTEIS_H_
+
+#include "animal.hpp"
+
+class Repteis: public Animal{
+
+
+
+};
