f68c82d66155d3875c0d290011b6a09381893b1d
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2020-11-19 19:41:40 -0300

    Create class Animal

---
+++ include/animal.hpp
@@ -0,0 +1,21 @@
+#ifndef _ANIMAL_H_
+#define _ANIMAL_H_
+
+class Animal{
+    protected:
+        string nome;
+        string sexo;
+        int idade;
+        float peso;
+        float comprimento;
+        string ambiente;//habitat
+        int patas;
+        string cor;
+        string alimento;
+        string ameacadoDeEx;
+        string silvestreOuExotico;
+        //vetResponsavel
+        //tratadorResponsavel
+
+};
+
