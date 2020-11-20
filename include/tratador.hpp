3cc6a57614acb854247cb6945e911ac8ef802c73
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2020-11-20 19:10:33 -0300

    update

---
+++ include/tratador.hpp
@@ -0,0 +1,13 @@
+#ifndef _TRATADOR_H_
+#define _TRATADOR_H_
+
+#include <iostream>
+#include "funcionario.hpp"
+
+class Tratador: public Funcionario{
+    public:
+        string nivel_seguranca;
+
+};
+
+#endif
---
+++ include/veterinario.hpp
@@ -0,0 +1,14 @@
+#ifndef _VETERINARIO_H_
+#define _VETERINARIO_H_
+
+#include <iostream>
+#include "funcionario.hpp"
+
+class Veterinario : public Funcionario{
+    public:
+        string crmv;
+};
+
+
+
+#endif
