5f1acfa1048e2da5cab9e9d603a29ed87a1614ac
JamiltonMB <jamilton.mb@gmail.com>
2020-11-21 19:07:26 -0300

    update

--- src/programa.cpp
+++ src/programa.cpp
@@ -5,3 +5,25 @@ Programa::Programa(){
 	tratadores.clear();
 }
 Programa::~Programa(){}
+
+void cadastrarVeterinario(int matricula, string nome, int idade, int celular, string endereco, string cpf, string cargo, string crmv){}
+
+void cadastrarTratador(int matricula, string nome, int idade, int celular, string endereco, string cpf, string cargo, string nivel_seguranca){}
+
+bool adicionarVeterinario(Veterinario* novo){
+	return true;
+}
+
+bool adicionarTratador(Tratador* novo){
+	return true;
+}
+
+bool removerFuncionario(int matricula){
+	return true;
+}
+
+void alterarFuncionario(int matricula){}
+
+void listarFuncionario(int matricula){}
+
+void listarTodosFuncionarios(){}
