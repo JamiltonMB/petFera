79ae921b03b30e13bd6cb1125dda156dabcc2e2a
JamiltonMB <jamilton.mb@gmail.com>
2020-11-21 10:30:11 -0300

    update

--- src/funcionario.cpp
+++ src/funcionario.cpp
@@ -1,28 +1,28 @@
 #include "funcionario.hpp"
 
-Funcionario::Funcionario(int matricula, string nome, int idade, int celular, string endereco, string cpf, string cargo):
+Funcionario::Funcionario(std::string matricula, std::string nome, int idade, std::string celular, std::string endereco, std::string cpf, std::string cargo):
 						matricula(matricula),nome(nome),idade(idade),celular(celular),endereco(endereco),cpf(cpf),cargo(cargo){}
 
 Funcionario::~Funcionario(){}
 
-int Funcionario::getMatricula(){
+std::string Funcionario::getMatricula(){
 	return this->matricula;
 }
-string Funcionario::getNome(){
+std::string Funcionario::getNome(){
 	return this->nome;
 }
 int Funcionario::getIdade(){
 	return this->idade;
 }
-int Funcionario::getCelular(){
+std::string Funcionario::getCelular(){
 	return this->celular;
 }
-string Funcionario::getEnderaco(){
+std::string Funcionario::getEnderaco(){
 	return this->endereco;
 }
-string Funcionario::getCpf(){
+std::string Funcionario::getCpf(){
 	return this->cpf;
 }
-string Funcionario::getCargo(){
+std::string Funcionario::getCargo(){
 	return this->cargo;
 }
