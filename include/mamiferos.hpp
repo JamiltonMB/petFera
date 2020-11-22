dc2f4a76500fbc4cbec08b5726936a5496d71b61
fbreno <fernandobrenofx@gmail.com>
2020-11-22 17:58:28 -0300

    Mamifero update

--- include/mamiferos.hpp
+++ include/mamiferos.hpp
@@ -10,7 +10,18 @@ class Mamiferos : public Animal{
         std::string dentes; //se possui dentes desenvolvidos
         //string tipo_mamifero;
     public:
+    Mamiferos(std::string cor_pelo,std::string dentes);
+    Mamiferos();
+    ~Mamiferos();
+    
     std::string getCorPelo();
+    void setCorPelo(std::string cor_pelo);
+
+    std::string getDentes();
+    void setDentes(std::string dentes);
+
+    std::ostream &Mamiferos::imprimirMamifero(std::ostream &os) const;
+
 };
 
 #endif
