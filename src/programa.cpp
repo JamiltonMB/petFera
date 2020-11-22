2dea8467792070b1285aa89185657467f20e214b
JamiltonMB <jamilton.mb@gmail.com>
2020-11-22 09:54:30 -0300

    testando funções

--- src/programa.cpp
+++ src/programa.cpp
@@ -9,6 +9,7 @@ Programa::~Programa(){}
 
 void Programa::cadastrarVeterinario(std::string matricula, std::string nome, int idade, std::string celular, std::string endereco, std::string cpf, std::string cargo, std::string crmv){
 	Veterinario* veterinario = new Veterinario(matricula, nome, idade, celular, endereco, cpf, cargo, crmv);
+	this->veterinarios.push_back(veterinario);
 }
 
 void cadastrarTratador(int matricula, std::string nome, int idade, int celular, std::string endereco, std::string cpf, std::string cargo, std::string nivel_seguranca){}
@@ -29,4 +30,15 @@ void alterarFuncionario(int matricula){}
 
 void Programa::listarFuncionario(int matricula){}
 
-void listarTodosFuncionarios(){}
+void Programa::listarTodosFuncionarios(){
+	
+	if (this->veterinarios.size()==0) {
+		std::cout << "Não há funcionários cadastrados." << std::endl;
+	} else {
+		for (auto& funcionario : this->veterinarios)
+		{
+			std::cout << (funcionario->getNome()) << std::endl;
+		}
+	}
+
+}
