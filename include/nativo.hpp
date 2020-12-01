25ed351a458108d5a17bff9b0abc841092ef241c
fbreno <fernandobrenofx@gmail.com>
2020-12-01 16:48:37 -0300

    nativo cpp add

--- include/nativo.hpp
+++ include/nativo.hpp
@@ -27,39 +27,3 @@ public:
     string getHabitat() const;
     void setHabitat(string habitat);
 };
-
-string Nativo::getLicenca_IBAMA()
-{
-    return licenca_IBAMA;
-}
-void Nativo::setLicenca_IBAMA(string licenca_IBAMA)
-{
-    this->licenca_IBAMA = licenca_IBAMA;
-}
-
-string Nativo::getUF_origem()
-{
-    return UF_origem;
-}
-void Nativo::setUF_origem(string UF_origem)
-{
-    this->UF_origem = UF_origem;
-}
-
-string Nativo::getAmeacadoDeEx() const
-{
-    return ameacadoDeEx;
-}
-void Nativo::setAmeacadoDeEx(string ameacadoDeEx)
-{
-    this->ameacadoDeEx = ameacadoDeEx;
-}
-
-string Nativo::getHabitat() const
-{
-    return habitat;
-}
-void Nativo::setHabitat(string habitat)
-{
-    this->habitat = habitat;
-}
