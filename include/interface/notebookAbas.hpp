60048c421d74a6d2004fe9b1b98f0bb2aadf2b4c
JamiltonMB <jamilton.mb@gmail.com>
2020-12-11 09:47:43 -0300

    modificações

--- include/interface/notebookAbas.hpp
+++ include/interface/notebookAbas.hpp
@@ -5,13 +5,15 @@
 #include <iostream>
 #include "veterinario.hpp"
 #include "tratador.hpp"
+#include "interface/abafuncionarios.hpp"
 
-GtkWidget *window;
-GtkWidget *notebook;
-GtkWidget *button, *grid, *frame, *box, *label, *label_veterinario, *label_tratador, *box_head, *box_base;
+GtkWidget *window, *notebook;
+GtkWidget *button, *grid, *frame, *box, *label, *box_head, *box_base;
 GtkWidget *radio1, *radio2;
-GtkWidget *entry_prod, *entry_cb, *entry_medida, *entry_qtd, *entry_preco;
-GtkWidget *entry_matricula, *entry_nome, *entry_idade, *entry_celular, *entry_endereco, *entry_cpf, *entry_cargo, *entry_crmv, *entry_nivel_seguranca;
+//GtkWidget *entry_prod, *entry_cb, *entry_medida, *entry_qtd, *entry_preco;
+GtkWidget *entry_matricula, *entry_nome, *entry_idade, *entry_celular, *entry_endereco, *entry_cpf, 
+*entry_cargo, *entry_crmv, *entry_nivel_seguranca, *label_veterinario, *label_tratador;
+
 
 void atualizarLista();
 
